7e15226b2cfeb9d503f0c679947b82c34fbb2cba
Server\NOT/founD <jirnobruh@vk.com>
2026-03-07 21:02:27 +0700

    Create Domains

---
+++ TestTaskAvalonia/Domain/Employee.cs
@@ -0,0 +1,10 @@
+namespace TestTaskAvalonia.Domain;
+
+public class Employee
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; } = "";
+    public string LastName { get; set; } = "";
+    public string Position { get; set; } = "";
+    public int OrganizationId { get; set; }
+}
---
+++ TestTaskAvalonia/Domain/Organization.cs
@@ -0,0 +1,8 @@
+namespace TestTaskAvalonia.Domain;
+
+public class Organization
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = "";
+    public string Address { get; set; } = "";
+}
